96b57a68cd6471da4daf6799ae1e0520165086dd
King Mangahas <mkamangahas@tip.edu.ph>
2026-03-12 16:23:54 +0800

    Initialize HydroAlert monorepo (admin, resident, official, shared_models)

---
+++ apps/admin_app/android/app/src/main/kotlin/com/example/hydroalert_flutter_application_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hydroalert_flutter_application_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/official_app/android/app/src/main/kotlin/com/example/official_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.official_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
